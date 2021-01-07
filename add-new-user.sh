d8f1c2e79ad54e128cf73d6f5e2160cc0826d4ce
TommyJonzen <tommyjonzenclarke@gmail.com>
2021-01-07 20:06:49 +0000

    Addin IO conventions for STDERR / STDOUT

--- add-new-user.sh
+++ add-new-user.sh
@@ -2,7 +2,7 @@
 
 # Check for root priveleges
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-  echo 'You must run this script with root priveleges'
+  echo 'You must run this script with root priveleges' 2> std.err | cat
   exit 1
 fi
 
@@ -11,11 +11,11 @@ HOW_TO_USE='Usage: add-new-user.sh username accountholders name [...]'
 
 # Check for correct no. of args
 if [[ "${#}" -lt 1 ]]; then
-  echo 'You must provide a Username'
+  echo 'You must provide a Username' 2> std.err | cat
   echo ${HOW_TO_USE}
   exit 1
 elif [[ "${#}" -lt 2 ]]; then
-  echo 'You must provide the account holders name'
+  echo 'You must provide the account holders name' 2> std.err | cat
   echo ${HOW_TO_USE}
   exit 1
 fi
@@ -32,17 +32,17 @@ useradd -c "${COMMENT}" -m ${USER_NAME}
 
 if [[ ${?} -ne 0 ]]
 then
-  echo 'Account creation failed at the useradd stage'
+  echo 'Account creation failed at the useradd stage' 2> std.err | cat
   exit 1
 fi
 
 # Set random password for user
-PASSWORD="$(echo ${RANDOM}$(date %s%N) | sha256sum | head -c 12)"
+PASSWORD="$(echo ${RANDOM}$(date +%s%N) | sha256sum | head -c 12)"
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 
 if [[ ${?} -ne 0 ]]
 then
-  echo 'Account created but password setup failed'
+  echo 'Account created but password setup failed' 2> std.err | cat
   exit 1
 fi
 
